2bb56c2463dd6278cf83cf85c01c706a7d805446
Sulaiman122 <33493055+Sulaiman122@users.noreply.github.com>
2021-11-11 10:24:10 +0300

    first result

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+// router
+const movieRouter = require("./routers/routes/moviesRoute");
+
+const app = express();
+app.use(cors())
+
+app.use(express.json());
+
+
+// const appMiddleware = (req, res, next) => {
+//   console.log("appMiddleware");
+//   next();
+// };
+// app.use(appMiddleware);
+
+// app.use(morgan("dev"));
+
+// const movieMiddleware = (req, res, next) => {
+//   console.log("movies");
+//   next();
+// };
+
+app.use("/movies", movieRouter);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server on ${PORT}`);
+});
